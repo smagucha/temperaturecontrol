785ef20e27c67b69bc68ee7609e828c1128bf6fd
smagucha <smagucha@gmail.com>
2022-02-08 18:51:28 +0300

    all changes

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import './home.css';
+import React, {useState} from 'react'
+function Home() {
+  const [temp, settemp] = useState(37)
+   const [color, setcolor] = useState('blue')
+
+
+    const increasetemp = ()=>{
+       if (temp > 37){
+        setcolor('red')
+      }
+      settemp(temp +1)
+    }
+    const decreasetemp = ()=>{
+      if (temp < 38){
+
+        setcolor('blue')
+      }
+      settemp(temp -1) 
+    }
+
+  return (
+    <div className="container">
+        <div className='tempvalue' style={{background:color}}>
+            {temp}°C
+        </div>
+        <div>
+         {/* <button onClick={()=>settemp(temp +1)}>+</button>
+          <button onClick={()=>settemp(temp -1)}>-</button>*/}
+          <button onClick={()=>increasetemp()}>+</button>
+          <button onClick={()=>decreasetemp()}>-</button>
+        </div>
+      
+    </div>
+  );
+}
+
+export default Home;
